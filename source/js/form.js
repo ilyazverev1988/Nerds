6737315cf3d70e248b77d73f4444233535642b11
ilyazverev1988 <ilya.verez@mail.ru>
2020-03-27 17:35:02 +0300

    27.03.2020

--- source/js/form.js
+++ source/js/form.js
@@ -65,4 +65,87 @@
         $sliderDesign.removeClass("visually-hidden");
     }
   });
+
+  pinHigh = document.querySelector(".filter__pin--high");
+  filterLine = document.querySelector(".filter__line");
+  filterLineSort = document.querySelector(".filter__line-sort");
+  costHigh = document.querySelector(".cost__high");
+  pinLow = document.querySelector(".filter__pin--low");
+  costLow = document.querySelector(".cost__low");
+
+  pinHigh.addEventListener("mousedown", function(evt) {
+    evt.preventDefault();
+
+    startCoords = {
+      x: evt.clientX
+    };
+
+    var onMouseMove = function(moveEvt) {
+      moveEvt.preventDefault();
+      var shift = { x: startCoords.x - moveEvt.clientX };
+
+      startCoords = { x: moveEvt.clientX };
+
+      var newPosition = pinHigh.offsetLeft - shift.x;
+      pinLow.style.left = 0 + "px";
+      if (newPosition > filterLine.offsetWidth) {
+        newPosition = filterLine.offsetWidth;
+      } else if (newPosition + "px" < pinLow.style.left) {
+        newPosition = pinLow.style.left;
+      }
+      pinHigh.style.left = newPosition + "px";
+      filterLineSort.style.width = newPosition + "px";
+
+      costHigh.value = Math.round(
+        (pinHigh.offsetLeft / filterLine.offsetWidth) * 23000
+      );
+    };
+
+    var onMouseUp = function() {
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    };
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+  });
+
+  pinLow.addEventListener("mousedown", function(evt) {
+    evt.preventDefault();
+
+    startCoords = {
+      x: evt.clientX
+    };
+
+    var onMouseMove = function(moveEvt) {
+      moveEvt.preventDefault();
+      var shift = { x: startCoords.x - moveEvt.clientX };
+
+      startCoords = { x: moveEvt.clientX };
+
+      var newPositionLow = pinLow.offsetLeft - shift.x;
+
+      if (newPositionLow > pinHigh.offsetLeft) {
+        newPositionLow = pinHigh.offsetLeft;
+      } else if (newPositionLow < 0) {
+        newPositionLow = 0;
+      }
+
+      pinLow.style.left = newPositionLow + "px";
+      filterLineSort.style.left = newPositionLow + "px";
+
+      console.log(filterLineSort.offsetWidth);
+      filterLineSort.style.width = costLow.value = Math.round(
+        (pinLow.offsetLeft / filterLine.offsetWidth) * 23000
+      );
+    };
+
+    var onMouseUp = function() {
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    };
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+  });
 })();
